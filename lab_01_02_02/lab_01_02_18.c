8ff04773accf664c57e2c7a03161651690675719
Romanov Simeon <simeon140202@gmail.com>
2021-03-14 00:05:17 +0300

    style fixe (number 2)

--- lab_01_02_02/lab_01_02_18.c
+++ lab_01_02_02/lab_01_02_18.c
@@ -14,7 +14,7 @@ int main()
 	return 0;
 } 
 
-double perimetr (double x1, double y1, double x2, double y2, double x3, double y3)
+double perimetr(double x1, double y1, double x2, double y2, double x3, double y3)
 {
 	double p = get_length(x1, x2, y1, y2) + get_length(x1, x3, y1, y3) + get_length(x3, x2, y3, y2);
 	return p;
