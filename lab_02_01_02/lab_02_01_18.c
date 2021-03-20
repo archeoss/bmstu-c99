c41162a97ff3d045adc0b0946777a39ddffbaa4c
Romanov Simeon <simeon140202@gmail.com>
2021-03-21 01:51:31 +0300

    number 2 fix №1

--- lab_02_01_02/lab_02_01_18.c
+++ lab_02_01_02/lab_02_01_18.c
@@ -3,21 +3,31 @@
 
 #define NO_ERRORS 0
 #define INPUT_ERROR 1
-
+float find_n(int *a, int n)
+{
+	float sr_n, negative_n = 0, negative_sum = 0;
+	for (int i = 0; i < n; i++)
+		if (a[i] < 0)
+			{
+				negative_sum += a[i];
+				negative_n += 1;
+			}
+	sr_n = negative_sum / negative_n;
+	return sr_n;
+}
 int main(void)
 {
 	int error_code = NO_ERRORS, rc;
-	int N;
-	float negative_sum = 0, negative_n = 0;
+	int n;
+	float sr_n;
 	setbuf(stdout, NULL);
 	printf("Введите количество: ");
-	rc = scanf("%d", &N);
+	rc = scanf("%d", &n);
 	printf("\n");
-	int a[N];
-	memset( a, 0, N*sizeof(int));
+	int a[n];
 	if (rc == 1)
 	{
-		for (int i = 0; i < N; i++)
+		for (int i = 0; i < n; i++)
 		{
 			printf("a[%d] = ", i);
 			rc = scanf("%d", &a[i]);
@@ -28,13 +38,8 @@ int main(void)
 			}
 			printf("\n");
 		}
-		for (int i = 0; i < N; i++)
-			if (a[i] < 0)
-			{
-				negative_sum += a[i];
-				negative_n += 1;
-			}
-		printf("Среднее арифметичексое = %f", negative_sum / negative_n);
+		sr_n = find_n(a, n);
+		printf("Среднее арифметичексое = %f", sr_n);
 	}
 	else
 		error_code = INPUT_ERROR;
