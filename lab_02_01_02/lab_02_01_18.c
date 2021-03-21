f8dd66173bc1ab5076bb49939c96de198393d994
Romanov Simeon <simeon140202@gmail.com>
2021-03-21 04:14:15 +0300

    number 1 fix no.3

--- lab_02_01_02/lab_02_01_18.c
+++ lab_02_01_02/lab_02_01_18.c
@@ -1,56 +1,85 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define N 10
+
 #define NO_ERRORS 0
 #define INPUT_ERROR 1
+#define INCORRECT_DATA 2
 
-float find_n(int *a, unsigned int n)
+int form_arr(int *a, int n)
+{
+	int rc, error_code = NO_ERRORS;
+	int tmp = 0; 
+	for (int i = 0; i < n; i++)
+		{
+			printf("a[%d] = ", i);
+			rc = scanf("%d", &tmp);
+			if (rc == EOF)
+				break;
+			if (rc == 0)
+			{
+				error_code = INPUT_ERROR;
+				break;
+			}
+			a[i] = tmp;
+			printf("\n");
+		}
+	return error_code;
+}
+
+
+float find_n(int *a, int n)
 {
 	float sr_n, negative_n = 0, negative_sum = 0;
-	for (unsigned int i = 0; i < n; i++)
+	for (int i = 0; i < n; i++)
 		if (a[i] < 0)
 		{
 			negative_sum += a[i];
 			negative_n += 1;
 		}
-	sr_n = negative_sum / negative_n;
+	if (negative_n <= 0)
+		sr_n = 0;
+	else
+		sr_n = negative_sum / negative_n;
 	return sr_n;
 }
+
 int main(void)
 {
 	int error_code = INPUT_ERROR, rc;
-	unsigned int n;
+	int n;
 	float sr_n;
 	setbuf(stdout, NULL);
 	printf("Введите количество: ");
-	rc = scanf("%u", &n);
+	rc = scanf("%d", &n);
 	printf("\n");
-	int * a = (int*)malloc(n * sizeof(int));
+	int a[N];
 	if (rc == 1)
 	{
-		int flag = 1;
-		for (unsigned int i = 0; i < n; i++)
+		if (n < 1 || n > 10)
+			error_code = INCORRECT_DATA;
+		else
 		{
-			printf("a[%u] = ", i);
-			rc = scanf("%d", &a[i]);
-			if (rc == 0)
+			error_code = form_arr(a, n);
+			if (error_code == NO_ERRORS)
 			{
-				error_code = INPUT_ERROR;
-				flag = 0;
-				break;
+				sr_n = find_n(a, n);
+				if (sr_n >= 0)
+					error_code = INCORRECT_DATA;
+				else
+				{
+					error_code = NO_ERRORS;
+					printf("Среднее арифметичексое = %f", sr_n);
+				}
 			}
-			printf("\n");
-		}
-		if (flag == 1)
-		{
-			sr_n = find_n(a, n);
-			printf("Среднее арифметичексое = %f", sr_n);
-			error_code = NO_ERRORS;
 		}
 	}
 	else
 		error_code = INPUT_ERROR;
 	if (error_code == INPUT_ERROR)
 		printf("INPUT_ERROR");
+	else if (error_code == INCORRECT_DATA)
+		printf("INCORRECT_DATA");
 	return error_code;
 }
