ee66f6f707b69e049d00d4e7b0e62129576ddf52
Romanov Simeon <simeon140202@gmail.com>
2021-05-30 23:56:27 +0300

    first iteration of no.1

---
+++ lab_05_01_00/lab_05_01_00.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "my_string.h"
+#define N 255
+
+#define NO_ERRORS 0
+#define INPUT_ERROR -1
+#define INCORRECT_DATA -2
+
+int process(FILE *f);
+
+int main(void)
+{
+	char file_in[N];
+	int error_code;
+	scanf("%s", file_in);
+	FILE *f;
+	if ((f = fopen(file_in,"r")) == NULL)
+		error_code = INPUT_ERROR;
+	else
+		error_code = process(f);
+	fclose(f);
+	if (error_code == INPUT_ERROR)
+		printf("INPUT_ERROR");
+	else if (error_code == INCORRECT_DATA)
+		printf("INCORRECT_DATA");
+	return error_code;
+}
+
+int process(FILE *f)
+{
+	int error_code = NO_ERRORS;
+	int max_n; 
+	max_n = find_pos_after_neg(f);
+	if (max_n == -1)
+		error_code = INCORRECT_DATA;
+	else
+		printf("%d", max_n);
+	return error_code;
+}
---
+++ lab_05_01_00/my_string.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int find_pos_after_neg(FILE *f)
+{
+	int max_n = -1, flag = 0;
+	int i = 0, rc;
+	while ((rc = fscanf(f, "%d ", &i)) != EOF && rc == 1)
+	{
+		if (i < 0)
+			flag = 1;
+		else if (flag == 1)
+		{
+			if (max_n < i)
+				max_n = i;
+			flag = 0;
+		}
+	}
+	if (rc == 0)
+		max_n = -1;
+	return max_n;
+}
