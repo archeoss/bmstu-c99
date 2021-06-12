0363fb340a6fbce683d84c930dc2829ef949504e
Romanov Simeon <simeon140202@gmail.com>
2021-06-12 21:18:30 +0300

    month check and get_int_date rework

--- lab_04_04_02/lab_04_04_18.c
+++ lab_04_04_02/lab_04_04_18.c
@@ -16,7 +16,7 @@ int get_int_date(char *a);
 
 int main(void)
 {
-	char months[][10] = { "january", "JANUARY", "february", "FEBRUARY", "march", "MARCH", "april", "APRIL", "may", "MAY", "june", "JUNE", "july", "JULY", "august", "AUGUST", "september","SEPTEMBER", "october", "OCTOBER", "november", "NOVEMBER", "december", "DECEMBER" };
+	char months[][10] = { "january", "february", "march", "april", "may", "june", "july", "august", "september", "october", "november", "december" };
 	int days[] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 	int error_code = NO_ERRORS;
 	int rc = -1;
@@ -24,39 +24,26 @@ int main(void)
 	char *arr1[MAX_LENGTH];
 	error_code = read_line(str1, MAX_LENGTH + 1);
 	get_words(str1, arr1);
-	char *p_m1;
-	char *p_m2;
-	char *p_p;
-	int i = 0;
+	int i = 0, j = 0;
 	int add = 0;
 	int year;
 	int day = 0;
+	char *p_m = arr1[1];
 	if (error_code == NO_ERRORS)
 	{
-		if (arr1[0] == NULL || arr1[1] == NULL || arr1[2] == NULL)
-			rc = INCORRECT_DATA;
+		while (*p_m)
+		{
+			*p_m = tolower(*p_m);
+			p_m++;
+		}
 		while (i < MONTH_COUNT && rc == -1)
 		{
-			p_m1 = months[i * 2];
-			p_m2 = months[i * 2 + 1];
-			p_p = arr1[1];
-			while (*p_m1 != '\0' && rc == -1)
-			{
-				if (*p_m1 != *p_p && *p_m2 != *p_p)
-					rc = INCORRECT_DATA;
-				else
-				{
-					p_m1++;
-					p_m2++;
-					p_p++;
-				}
-			}
-			if (rc == -1 && *p_p == '\0')
+			if (strcmp(months[i], arr1[1]) == 0)
 				rc = NO_ERRORS;
-			else
-				rc = -1;
 			i++;
 		}
+		if (i == MONTH_COUNT && rc == -1)
+			rc = INCORRECT_DATA;
 		i--;
 		if (rc == NO_ERRORS)
 		{
@@ -105,7 +92,7 @@ int get_int_date(char *a)
 {
 	int date = 0;
 	int flag = 0;
-	while (*a != '\0' && flag == 0)
+	while (*a && flag == 0)
 	{
 		if (isdigit(*a) != 0)
 		{
