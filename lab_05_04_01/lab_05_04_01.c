972a5efb3a8b4bba9447d8ce2b3dd19fbe060ed3
Romanov Simeon <simeon140202@gmail.com>
2021-06-13 22:18:10 +0300

    test

--- lab_05_04_01/lab_05_04_01.c
+++ lab_05_04_01/lab_05_04_01.c
@@ -25,7 +25,7 @@ int main(int args, char **keys)
 {
 	int error_code = NO_ERRORS;
 	FILE *f;
-	FILE *f_out;
+	//FILE *f_out;
 	char known_keys[3][3] = { "sb", "fb", "db" };
 	if (args > 5 || args < 3)
 		error_code = UNKOWN_KEY;
@@ -46,41 +46,41 @@ int main(int args, char **keys)
 				fclose(f);
 			}
 		}
-		else if (args == 5 && strcmp(keys[1], known_keys[1]) == 0)
-		{
-			f = fopen(keys[2], "rb");
-			f_out = fopen(keys[3], "wb");
-			if (f == NULL)
-			{		
-				error_code = INPUT_ERROR;
-			}
-			else
-			{
-				error_code = get_students_by_substr(f, f_out, keys[4]);
-				fclose(f);
-			}
-			fclose(f_out);
-		}
-		else if (args == 3 && strcmp(keys[1], known_keys[2]) == 0)
-		{
-			f = fopen(keys[2], "rb");
-			if (f == NULL)
-			{		
-				error_code = INPUT_ERROR;
-			}
-			else
-			{
-				char tmp_f[] = { "temp.bin" };
-				FILE *f_temp = fopen(tmp_f, "w+b");
-				error_code = delete_under_avg(f, f_temp);
-				fclose(f);
-				f = fopen(keys[2], "wb");
-				f_copy(f, f_temp);
-				fclose(f_temp);
-				remove(tmp_f);
-				fclose(f);
-			}
-		}
+		// else if (args == 5 && strcmp(keys[1], known_keys[1]) == 0)
+		// {
+			// f = fopen(keys[2], "rb");
+			// f_out = fopen(keys[3], "wb");
+			// if (f == NULL)
+			// {		
+				// error_code = INPUT_ERROR;
+			// }
+			// else
+			// {
+				// error_code = get_students_by_substr(f, f_out, keys[4]);
+				// fclose(f);
+			// }
+			// fclose(f_out);
+		// }
+		// else if (args == 3 && strcmp(keys[1], known_keys[2]) == 0)
+		// {
+			// f = fopen(keys[2], "rb");
+			// if (f == NULL)
+			// {		
+				// error_code = INPUT_ERROR;
+			// }
+			// else
+			// {
+				// char tmp_f[] = { "temp.bin" };
+				// FILE *f_temp = fopen(tmp_f, "w+b");
+				// error_code = delete_under_avg(f, f_temp);
+				// fclose(f);
+				// f = fopen(keys[2], "wb");
+				// f_copy(f, f_temp);
+				// fclose(f_temp);
+				// remove(tmp_f);
+				// fclose(f);
+			// }
+		// }
 		else
 			error_code = UNKOWN_KEY;
 	}
