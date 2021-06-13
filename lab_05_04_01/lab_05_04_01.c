d841f54a64747c0943c67a1f8364a28c921a9607
Romanov Simeon <simeon140202@gmail.com>
2021-06-13 19:05:49 +0300

    test

--- lab_05_04_01/lab_05_04_01.c
+++ lab_05_04_01/lab_05_04_01.c
@@ -26,7 +26,7 @@ int main(int args, char **keys)
 {
 	int error_code = NO_ERRORS;
 	FILE *f;
-	FILE *f_out;
+	//FILE *f_out;
 	char known_keys[3][3] = { "sb", "fb", "db" };
 	if (args > 5 || args < 3)
 		error_code = UNKOWN_KEY;
@@ -49,23 +49,23 @@ int main(int args, char **keys)
 				fclose(f);
 			}
 		}
-		else if (args == 5 && strcmp(keys[1], known_keys[1]) == 0)
-		{
-			f = fopen(keys[2], "rb");
-			f_out = fopen(keys[3], "wb");
-			if (f == NULL)
-			{		
-				error_code = INPUT_ERROR;
-			}
-			else if(getlen(f) < 1)
-				error_code = INCORRECT_DATA;
-			else
-			{
-				error_code = get_students_by_substr(f, f_out, keys[4]);
-				fclose(f);
-			}
-			fclose(f_out);
-		}
+		// else if (args == 5 && strcmp(keys[1], known_keys[1]) == 0)
+		// {
+			// f = fopen(keys[2], "rb");
+			// f_out = fopen(keys[3], "wb");
+			// if (f == NULL)
+			// {		
+				// error_code = INPUT_ERROR;
+			// }
+			// else if(getlen(f) < 1)
+				// error_code = INCORRECT_DATA;
+			// else
+			// {
+				// error_code = get_students_by_substr(f, f_out, keys[4]);
+				// fclose(f);
+			// }
+			// fclose(f_out);
+		// }
 		// else if (args == 3 && strcmp(keys[1], known_keys[2]) == 0)
 		// {
 			// error_code = delete_under_avg(keys[2]);
