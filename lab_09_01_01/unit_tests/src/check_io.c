f82fb231315b43fff2ec1aaa7d44f017e9d6c58c
archeoss <archeosAnacritis@gmail.com>
2021-12-27 07:40:17 +0300

    unit_tests fix

--- lab_09_01_01/unit_tests/src/check_io.c
+++ lab_09_01_01/unit_tests/src/check_io.c
@@ -38,56 +38,56 @@ START_TEST(correct_read_author)
 END_TEST
 
 //
-// find_item() check
+// print_item() check
 //
 
-START_TEST(find_item_title)
+START_TEST(print_item_title)
 {
     FILE *f = fopen("./unit_tests/tests/test_01.txt", "r");
     movie_struct *movie = malloc(sizeof(movie_struct) * 3);
     read_items(f, movie, TITLE_MODE);
     char test[] = "Titanic";
     if (f)
-        ck_assert_int_eq(find_item(movie, test, 3, TITLE_MODE), 0);
+        ck_assert_int_eq(print_item(movie, test, 3, TITLE_MODE), 0);
     clear_array(movie, 3);
     fclose(f);
 }
 END_TEST
 
-START_TEST(find_item_year)
+START_TEST(print_item_year)
 {
     FILE *f = fopen("./unit_tests/tests/test_01.txt", "r");
     movie_struct *movie = malloc(sizeof(movie_struct) * 3);
     read_items(f, movie, YEAR_MODE);
     char test[] = "1980";
     if (f)
-        ck_assert_int_eq(find_item(movie, test, 3, YEAR_MODE), 0);
+        ck_assert_int_eq(print_item(movie, test, 3, YEAR_MODE), 0);
     clear_array(movie, 3);
     fclose(f);
 }
 END_TEST
 
-START_TEST(find_item_author)
+START_TEST(print_item_author)
 {
     FILE *f = fopen("./unit_tests/tests/test_01.txt", "r");
     movie_struct *movie = malloc(sizeof(movie_struct) * 3);
     read_items(f, movie, AUTHOR_MODE);
     char test[] = "Rob Reiner";
     if (f)
-        ck_assert_int_eq(find_item(movie, test, 3, AUTHOR_MODE), 0);
+        ck_assert_int_eq(print_item(movie, test, 3, AUTHOR_MODE), 0);
     clear_array(movie, 3);
     fclose(f);
 }
 END_TEST
 
-START_TEST(find_item_notfound)
+START_TEST(print_item_notfound)
 {
     FILE *f = fopen("./unit_tests/tests/test_01.txt", "r");
     movie_struct *movie = malloc(sizeof(movie_struct) * 3);
     char test[] = "Robby";
     read_items(f, movie, AUTHOR_MODE);
     if (f)
-        ck_assert_int_eq(find_item(movie, test, 3, AUTHOR_MODE), NOT_FOUND);
+        ck_assert_int_eq(print_item(movie, test, 3, AUTHOR_MODE), NOT_FOUND);
     clear_array(movie, 3);
     fclose(f);
 }
@@ -107,10 +107,10 @@ Suite *io_suite(void)
     tcase_add_test(tc, correct_read_title);
     tcase_add_test(tc, correct_read_year);
 
-    tcase_add_test(tc, find_item_author);
-    tcase_add_test(tc, find_item_title);
-    tcase_add_test(tc, find_item_year);
-    tcase_add_test(tc, find_item_notfound);
+    tcase_add_test(tc, print_item_author);
+    tcase_add_test(tc, print_item_title);
+    tcase_add_test(tc, print_item_year);
+    tcase_add_test(tc, print_item_notfound);
     
     suite_add_tcase(s, tc);
     return s;
